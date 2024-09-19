e61cb5bba26ed7ebd2e63b719e469be7a1b8a822
Sarosij Ghosh Ray <125396114+Sarosij@users.noreply.github.com>
2024-09-19 12:54:04 +0530

    Update AiAssistantButton.cs

--- Assets/Scripts/AiAssistantButton.cs
+++ Assets/Scripts/AiAssistantButton.cs
@@ -5,19 +5,19 @@ using UnityEngine;
 public class AiAssistantButton : MonoBehaviour
 {
     public GameObject AiTextbox;
-    public GameObject AiTextbox2; // Add this line
+    public GameObject AiTextbox2;
     public KeyCode toggleKey = KeyCode.Tab;
-    public KeyCode toggleKey2 = KeyCode.Q; // Add this line
+    public KeyCode toggleKey2 = KeyCode.Q;
 
     void Update()
     {
         if (Input.GetKeyDown(toggleKey))
         {
             AiTextbox.SetActive(!AiTextbox.activeSelf);
-            AiTextbox2.SetActive(false); // Add this line
+            AiTextbox2.SetActive(false);
         }
 
-        if (Input.GetKeyDown(toggleKey2)) // Add this block
+        if (Input.GetKeyDown(toggleKey2))
         {
             AiTextbox2.SetActive(!AiTextbox2.activeSelf);
             AiTextbox.SetActive(false);
